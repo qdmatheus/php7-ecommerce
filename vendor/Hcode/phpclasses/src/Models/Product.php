e72a57768af07793e829a22641059de6d5b00cf7
Matheus Quadros <matheus.qo@gmail.com>
2023-05-24 21:32:10 -0300

    atualizando 240523

---
+++ vendor/Hcode/phpclasses/src/Models/Product.php
@@ -0,0 +1,69 @@
+<?php
+//remodelar esse codigo para Product conforme aula 111
+namespace Hcode\Models;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
+
+class Category extends Model {
+
+    public static function listAll() 
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+        
+        $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+            ":idcategory"=>$this->getidcategory(),
+            ":descategory"=>$this->getdescategory()
+        ));
+
+        $this->setData($results[0]);
+
+        Category::updateFile();
+    }
+
+    public function get($idcategory)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+            ":idcategory"=>$idcategory
+        ]);
+
+        $this->setData($results[0]);
+    }
+    
+    public function delete()
+    {
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+            ":idcategory"=>$this->getidcategory()
+        ]);
+
+        Category::updateFile();
+    }
+
+    public static function updateFile()
+    {
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/php7-ecommerce/index.php/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "php7-ecommerce" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+    }
+
+}
+
+?>
