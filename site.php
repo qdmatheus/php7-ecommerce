b0cd2480f189bca540ec3a3186218bc565b5fec3
Matheus Quadros <matheus.qo@gmail.com>
2023-05-30 18:58:32 -0300

    atualizando 300523

--- site.php
+++ site.php
@@ -18,15 +18,44 @@ $app->get('/', function() {
 
 $app->get("/categories/:idcategory", function($idcategory){
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i = 1; $i <= $pagination['pages']; $i++){
+		array_push($pages, [
+			"link"=>'categories/' . $category->getidcategory() . '?page=' . $i,
+			"page"=>$i
+		]);
+	}
 
 	$page = new Page();
 
 	$page->setTpl("category", [
 		"category"=>$category->getValues(),
-		"products"=>Product::checkList($category->getProducts())
+		"products"=>$pagination["data"],
+		"pages"=>$pages
+	]);
+
+});
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		"product"=>$product->getValues(),
+		"categories"=>$product->getCategories()
 	]);
 
 });
