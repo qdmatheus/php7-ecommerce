de38ec7d0bd28ae0b013fa49fb50eb95ae87bc51
Matheus Quadros <matheus.qo@gmail.com>
2023-07-08 11:25:58 -0300

    atualizando 080723

--- vendor/Hcode/phpclasses/src/Models/Order.php
+++ vendor/Hcode/phpclasses/src/Models/Order.php
@@ -8,6 +8,9 @@ use \Hcode\Models\Cart;
 
 class Order extends Model {
 
+    const SUCCESS = "Order-Success";
+    const ERROR = "Order-Error";
+
     public function save()
     {
 
@@ -95,6 +98,56 @@ class Order extends Model {
         return $cart;
 
     }
+
+    public static function setError($msg)
+	{
+
+		$_SESSION[Order::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[Order  ::ERROR]) && $_SESSION[Order  ::ERROR]) ? $_SESSION[Order   ::ERROR] : '';
+
+		Order  ::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[Order::ERROR] = NULL;
+
+	}
+
+    public static function setSuccess($msg)
+	{
+
+		$_SESSION[Order::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess()
+	{
+
+		$msg = (isset($_SESSION[Order  ::SUCCESS]) && $_SESSION[Order::SUCCESS]) ? $_SESSION[Order   ::SUCCESS] : '';
+
+		Order  ::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess()
+	{
+
+		$_SESSION[Order::SUCCESS] = NULL;
+
+	}
 
 }
 
